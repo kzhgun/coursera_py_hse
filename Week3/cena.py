15f846f5d84d50212d33344e1d530acc3569fd4e
Ksu <zhgunksenia@gmail.com>
2020-05-04 22:31:44 +0300

    v5

--- Week3/cena.py
+++ Week3/cena.py
@@ -1,6 +1,19 @@
-a = float(input())
-b = int(a)
-d = a - b
-c = int(round(d, 2)*100)
-print(f'{b} {c}')
-print(b)
+
+
+n = int(input())
+a = []
+
+for _ in range(n):
+    a.append(float(input()))
+print(a)
+b = int(input())
+
+g = []
+for _ in range(2*b):
+    for _ in range(2):
+        g.append(int(input()))
+        print(g)
+    h = a[g[0]:g[1+1]]
+    print(h)
+
+
